3f6689f566c45a7bbf0cefa3fec3ff7869ef3592
Rabbid76 <gernot.steinegger@gmail.com>
2023-07-16 18:09:02 +0200

    feat: examble - embind pass array

--- examples/c++/corto/corto_em_test/bind_load_file.ps1
+++ examples/c++/corto/corto_em_test/bind_load_file.ps1
@@ -1,3 +1,4 @@
+#Set-PSDebug -Trace 1
 $source_file = "bind_load_file"
 emcc --bind "$($source_file).cpp" -o "./../serve/$($source_file).html" -s WASM=1 --shell-file "$($source_file).html"
 start "http://localhost:8000/$($source_file).html"

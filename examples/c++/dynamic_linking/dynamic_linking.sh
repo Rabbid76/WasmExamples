508906ff179bf621053e53218a5e9d3147a2398e
Gernot Steinegger <gernot.steinegger@roomle.com>
2023-08-01 08:16:56 +0200

    feat: dynamic linking example

--- examples/c++/dynamic_linking/dynamic_linking.sh
+++ examples/c++/dynamic_linking/dynamic_linking.sh
@@ -3,16 +3,27 @@
 source="dynamic_linking"
 emscripten_folder="/Users/$USER/source/emscripten/emsdk/upstream/emscripten"
 
+$emscripten_folder/emcc \
+    hello.cpp \
+    -o hello.wasm \
+    -s SIDE_MODULE=1
+
+$emscripten_folder/emcc \
+    hello_world.cpp \
+    -o hello_world.wasm \
+    -s SIDE_MODULE=1  
 
 $emscripten_folder/emcc \
     --no-entry \
     --bind \
     -O0 \
-    hello.cpp \
-    hello_world.cpp \
+    hello.wasm \
+    hello_world.wasm \
     $source.cpp \
     -o $source.js \
-    -s WASM=1
+    -s WASM=1 \
+    -s MAIN_MODULE=1
+
 
 echo "http://localhost:8090/$source.html"
 python3 -m http.server 8090 --directory "./"

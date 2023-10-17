6d4b88586b72fa3288d5132621d63c9453f037a4
Gernot Steinegger <gernot.steinegger@roomle.com>
2023-10-17 20:31:51 +0200

    ci: hello world modularaze build script

---
+++ examples/c++/hello_world/hello_world_modularize/hello_world_modularize.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source="hello_world_modularize"
+emscripten_folder="/Users/$USER/source/emscripten/emsdk/upstream/emscripten"
+
+$emscripten_folder/emcc --no-entry --bind -O0 $source.cpp -o $source.js -s WASM=1 -s MODULARIZE=1 -s EXPORT_NAME="createMyModule"
+
+echo "http://localhost:8090/$source.html"
+python3 -m http.server 8090 --directory "./"
